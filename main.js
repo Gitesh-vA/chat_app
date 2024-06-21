54e1e495ff88ee52317a40854dae5c309c49812e
Gitesh Yadav <Gitesh Yadav>
2024-06-21 19:18:09 +0530

    Updates

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createWindow() {
     // isDev
     //   ? 'https://chat-app-flame-rho.vercel.app/'
     //   : `file://${path.join(__dirname, '../build/index.html')}`
-    'https://chat-6qammwo7h-gitesh-developers-projects.vercel.app'
+    'https://chat-p370dmpld-gitesh-developers-projects.vercel.app'
   )
 
   win.webContents.openDevTools();
